3faa68c9b02869e57acf1118240b31ff9c3d0c1e
JMoney-216 <jdm216@gmail.com>
2019-10-30 15:39:23 -0500

    minimum features implemented, plus input clears on click, formatting looks good

---
+++ js/main.js
@@ -0,0 +1,17 @@
+//add li
+$(document).ready(function() {
+    $('#newSkill').click(function() {
+        var myList = $('#text').val();
+        $('#list').append('<li>'+'<button id="remove">'+'X'+'</button>'+myList+'</li>');
+        return false;
+    });
+});
+$(document).ready(function() {
+    $('#newSkill').click(function() {
+        $('#text').val('');
+    })
+});
+
+$(document).on('click','li', function(){
+    $(this).remove();    
+  });
